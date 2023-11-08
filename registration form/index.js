6c287bdb5884dd0f35b8bec653dd3bba5f9c0b4e
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2023-11-08 13:31:27 +0530

    added edit button

--- registration form/index.js
+++ registration form/index.js
@@ -4,20 +4,36 @@ let email = document.getElementById("email");
 let phone = document.getElementById("phone");
 let displayArea = document.getElementById("displayData");
 
+let editKey = null;
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const formDetail = {
-        name: Name.value,
-        email: email.value,
-        phone: phone.value,
-    };
+    if (editKey !== null) {
+        // If in edit mode, update the existing data
+        const formData = JSON.parse(localStorage.getItem(editKey));
+        formData.name = Name.value;
+        formData.email = email.value;
+        formData.phone = phone.value;
+        localStorage.setItem(editKey, JSON.stringify(formData));
+        editKey = null;
+    } else {
+        // If not in edit mode, create a new entry
+        const formDetail = {
+            name: Name.value,
+            email: email.value,
+            phone: phone.value,
+        };
 
-    // Generate a unique key using a timestamp
-    const key = "formData_" + Date.now();
+        // Generate a unique key using a timestamp
+        const key = "formData_" + Date.now();
 
-    // Store the form data with the unique key
-    localStorage.setItem(key, JSON.stringify(formDetail));
+        // Store the form data with the unique key
+        localStorage.setItem(key, JSON.stringify(formDetail));
+    }
+
+    // Reset the form fields
+    form.reset();
 
     // Display the stored data
     displayStoredData();
@@ -35,11 +51,26 @@ function displayStoredData() {
             <p>Name: ${formData.name}</p>
             <p>Email: ${formData.email}</p>
             <p>Phone: ${formData.phone}</p>
+            <button class="edit-button" data-key="${key}">Edit</button>
             <button class="delete-button" data-key="${key}">Delete</button>
         `;
 
         displayArea.appendChild(displayDataDiv);
     }
+
+    // Add event listeners to the edit buttons
+    const editButtons = document.querySelectorAll(".edit-button");
+    editButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            editKey = this.getAttribute("data-key");
+            const formData = JSON.parse(localStorage.getItem(editKey));
+
+            // Populate the form with data for editing
+            Name.value = formData.name;
+            email.value = formData.email;
+            phone.value = formData.phone;
+        });
+    });
 
     // Add event listeners to the delete buttons
     const deleteButtons = document.querySelectorAll(".delete-button");
